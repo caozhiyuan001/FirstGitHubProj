219dcc502ae8c78b03fa6e766908bdbd8f319dbb
caozhiyuan001 <44341080+caozhiyuan001@users.noreply.github.com>
2018-10-21 22:20:10 +0800

    Add files via upload

---
+++ Test10.java
@@ -0,0 +1,18 @@
+package kgc.Test;
+
+public class Test10 {
+	//输出数字三角形**
+	public static void main(String[] args) {
+		int n = 8;
+		for(int i=0;i<=n;i++) {
+			for(int j=0; j<=n-i ; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<2*i+1;k++) {
+				System.out.print(i+1);
+			}
+			System.out.println();
+		}
+	}
+
+}
